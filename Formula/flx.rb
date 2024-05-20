e4b204e4b5f2c34008ad1dc9a6fe953c4e6599c2
Takeshi Tsukamoto <dev@itome.team>
2024-05-20 16:46:21 +0900

    Bump up version

--- Formula/flx.rb
+++ Formula/flx.rb
@@ -1,9 +1,9 @@
 class Flx < Formula
   desc "Flutter DevTools TUI"
   homepage "https://github.com/itome/flx"
-  url "https://github.com/itome/flx/releases/download/0.1.8/flx-0.1.8-macos-arm64.tar.gz"
-  sha256 "88fdaa68e8b53a2d004d594368b24af0d6cb34df9f4f48228f32d9d12d533ccc"
-  version "0.1.8"
+  url "https://github.com/itome/flx/releases/download/0.1.9/flx-0.1.9-macos-arm64.tar.gz"
+  sha256 "f39ee98431720f6d26da358273c3b3be56b0b8b9344ae0e04b70afc758968d35"
+  version "0.1.9"
 
   def install
     bin.install "flx"
